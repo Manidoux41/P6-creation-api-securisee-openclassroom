64c1a6451dbac3f06caaf0d3262a3905391a5eb5
superflyman90 <superflyman90@gmail.com>
2022-12-19 08:16:05 +0100

    modif api main route

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ const PORT = process.env.PORT || 3001
 
 const app = express();
 
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+// Routes
+app.get('/', (req,res) => {
+    res.send('<h2>Welcome on the main API page</h2>')
+})
 
 app.listen(PORT, () => console.log(`Server Listening on port : ${PORT}`))
